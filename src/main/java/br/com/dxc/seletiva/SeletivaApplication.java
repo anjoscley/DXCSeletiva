65b50817ee83c6b7e830262b6c8050b1e2823ce9
Dariclei Anjos <anjoscley@gmail.com>
2025-01-07 15:06:31 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/dxc/seletiva/SeletivaApplication.java
@@ -0,0 +1,13 @@
+package br.com.dxc.seletiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeletivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeletivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dxc/seletiva/SeletivaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dxc.seletiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeletivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
